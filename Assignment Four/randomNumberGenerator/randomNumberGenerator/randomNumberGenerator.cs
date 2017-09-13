377e29e27e15a0dc1af9e50d83b51564a3f68375
Jmackas <jamesrossmackay@gmail.com>
2017-09-13 18:15:07 +0800

    Update

--- Assignment Four/randomNumberGenerator/randomNumberGenerator/randomNumberGenerator.cs
+++ Assignment Four/randomNumberGenerator/randomNumberGenerator/randomNumberGenerator.cs
@@ -50,43 +50,38 @@ namespace randomNumberGenerator
                 // Output file creation 
                 outputFile = File.CreateText("Numbers.Text");
 
-                // While 
-                
-
 
                 SaveFileDialog saveFileControl = new SaveFileDialog();
 
                 // Forcing the user to output the file type '.txt'
                 saveFileControl.Filter = "Text Files | *.txt";
-                saveFileControl.FilterIndex = 2;
-                saveFileControl.RestoreDirectory = true;
 
+                // If statement that opens the save dialog
                 if (saveFileControl.ShowDialog() == DialogResult.OK)
                 {
+                    // Save dialog to create a new file
                     using (Stream s = File.Open(saveFileControl.FileName, FileMode.CreateNew))
                     using (StreamWriter sw = new StreamWriter(s))
                     {
+                        // Loop that outputs the randomly generated numbers to file
                         while (count <= maxNum)
                         {
+                            // The counter variable (declared initially above)
                             count++;
+                            // Output of the randomly generated + line space
                             sw.Write(rand.Next(100) + Environment.NewLine);
                         }
-                        
+
                     }
                 }
 
-
+                // Exit of the saving 
                 outputFile.Close();
-
-
-
-
-
-
             }
 
             catch
             {
+                // Fallback to a broken 
                 MessageBox.Show("Please enter an integer value.");
             }
         }
