53225f903a85442b203757fddf89f0d4ef2c56ac
Jmackas <jamesrossmackay@gmail.com>
2017-10-02 19:46:51 +0800

    Touches

--- Assignment 5/Simple Addition Quiz/Simple Addition Quiz/Form1.cs
+++ Assignment 5/Simple Addition Quiz/Simple Addition Quiz/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Input validation isn't essential for this, as the program will remind the user an error is present. 
+
 namespace Simple_Addition_Quiz
 {
     public partial class simpleAdditionQuizForm : Form
@@ -18,29 +20,34 @@ namespace Simple_Addition_Quiz
         public simpleAdditionQuizForm()
         {
             InitializeComponent();
-            // Loads the intial random numbers
+            
+            // Loads the intial random numbers generated
             preloadData();
         }
 
         private void solutionGenerator_Click(object sender, EventArgs e)
         {
-            // Select 
+            // Start the algorithm to check the user's answer
             buttonSelectTwo_Click();
+
+            // Restart form to reset the values
             Application.Restart();
         }
 
         private void preloadData()
         {
+            // No try catch necessary as no user input therefore no exceptions expected
+
                 // Initialisation of the two random addition variables
                 int randomNumOne, randomNumTwo;
 
                 // Generate random numbers
                 Random rand = new Random();
 
-                // Generate random number between 0-100 and assign to randNumOne variable
+                // Generate random number between 100-500 and assign to randNumOne variable
                 randomNumOne = rand.Next(100, 500 + 1);
 
-                // Generate random number between 0-100 and assign to randNumOne variable
+                // Generate random number between 100-500 and assign to randNumOne variable
                 randomNumTwo = rand.Next(100, 500 + 1);
 
                 // Output the random number to randomNumOneLabel
@@ -51,33 +58,36 @@ namespace Simple_Addition_Quiz
 
                 // Formula to find the result of the random number
                 correctResultNumber = randomNumOne + randomNumTwo;
-
         }
 
         private void buttonSelectTwo_Click()
         {
             try
             {
-                // Initialisation of the input variable
+                // Initialisation of the user input variable
                 int inputNumberText;
 
                 // Conversion of input string to integer
                 int.TryParse(inputNumberTextBox.Text, out inputNumberText);
 
+                // If-else to determine if the user input is correct
                 if (inputNumberText == correctResultNumber)
                 {
+                    // If user answer is correct, then display 'Correct'
                     MessageBox.Show("Correct");
                 }
 
                 else
                 {
+                    // If user answer is not correct, then tell them the answer is incorrect, and display the correct answer
                     MessageBox.Show("Incorrect, the answer is " + correctResultNumber);
                 }
             }
 
             catch
             {
-                MessageBox.Show("Please enter an integer value.");
+                // Catch potential error if an exception is thrown via the user input
+                MessageBox.Show("Please enter a whole, positive, number.");
             }
 
         }
