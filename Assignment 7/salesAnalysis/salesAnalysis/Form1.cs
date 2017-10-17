217fa02c91b397963c8ba9344c4931f54f92f5da
James <jamesrossmackay@gmail.com>
2017-10-17 08:21:55 +0800

    comments

--- Assignment 7/salesAnalysis/salesAnalysis/Form1.cs
+++ Assignment 7/salesAnalysis/salesAnalysis/Form1.cs
@@ -93,10 +93,8 @@ namespace salesAnalysis
             /* Converting the string to a declared array */
             string[] listBoxToArray = numberList.Select(c => c.ToString()).ToArray();
 
-            /* Converting the string array to a decimal array */
+            /* Converting the string array to an int array */
             int[] myInts = Array.ConvertAll(listBoxToArray, int.Parse);
-
-
         }
 
         /* Method for sorting the code */
